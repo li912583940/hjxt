66b1a111dbb9431e74ec55a9695448c2920c5c25
li912583940 <912583940@qq.com>
2018-04-02 22:39:02 +0800

    web 配置

--- hjSystem/src/main/java/com/sl/ue/web/UserWeb.java
+++ hjSystem/src/main/java/com/sl/ue/web/UserWeb.java
@@ -1,5 +1,21 @@
 package com.sl.ue.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 说明 [用户]
+ * L_晓天  @2018年4月2日
+ */
+@Controller
 public class UserWeb {
 
+	@RequestMapping("/hello")
+	public ModelAndView hello(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("hello");
+		return mv;
+	}
 }
