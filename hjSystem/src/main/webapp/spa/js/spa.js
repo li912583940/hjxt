cb285623aba91b675e08077d0ec2d90ab9c92688
li912583940 <912583940@qq.com>
2018-04-30 14:20:29 +0800

    web

--- hjSystem/src/main/webapp/spa/js/spa.js
+++ hjSystem/src/main/webapp/spa/js/spa.js
@@ -1,4 +1,4 @@
-var spa = (function(){
+/*var spa = (function(){
 				var initModule = function ($container) {
 					$container.html(
 						'<h1 style="display:inline-block; margin:25px;">'
@@ -8,4 +8,28 @@ var spa = (function(){
 				};
 				
 				return { initModule: initModule };
-			}());
+			}());*/
+
+var app_url = $(function(){  
+    //获取当前网址，如： http://localhost:80/ybzx/index.jsp  
+    var curPath=window.document.location.href;  
+    //获取主机地址之后的目录，如： ybzx/index.jsp  
+    var pathName=window.document.location.pathname;  
+    var pos=curPath.indexOf(pathName);  
+    //获取主机地址，如： http://localhost:80  
+    var localhostPaht=curPath.substring(0,pos);  
+    //获取带"/"的项目名，如：/ybzx
+    var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);  
+    var path = localhostPaht+projectName;
+    console.log(path);
+    Window.appUrl = path;
+    return path;
+   
+}); 
+
+var spa = (function(){
+	var initModule = function($container){
+		spa.shell.initModule($container);
+	};
+	return { initModule : initModule };
+}());
