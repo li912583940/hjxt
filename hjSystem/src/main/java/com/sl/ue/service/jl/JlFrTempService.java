51d32f463642f4c07f4548f47a65e9e6af7b3245
li912583940 <912583940@qq.com>
2018-05-20 23:20:35 +0800

    web

--- hjSystem/src/main/java/com/sl/ue/service/jl/JlFrTempService.java
+++
@@ -1,8 +0,0 @@
-package com.sl.ue.service.jl;
-
-import com.sl.ue.entity.jl.JlFrTemp;
-import com.sl.ue.service.BaseService;
-
-public interface JlFrTempService extends BaseService<JlFrTemp>{
-
-}
--- hjSystem/src/main/java/com/sl/ue/service/sys/SysHjVideoService.java
+++
@@ -1,8 +0,0 @@
-package com.sl.ue.service.sys;
-
-import com.sl.ue.entity.sys.SysHjVideo;
-import com.sl.ue.service.BaseService;
-
-public interface SysHjVideoService extends BaseService<SysHjVideo>{
-
-}
