74aaaa21da75f17b208ddb602343085b3737c4e6
li912583940 <912583940@qq.com>
2018-05-28 00:08:32 +0800

    登录

--- HjSystem/src/main/java/com/sl/ue/web/sys/SysUserWeb.java
+++ HjSystem/src/main/java/com/sl/ue/web/sys/SysUserWeb.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializeFilter;
-import com.alibaba.fastjson.serializer.SerializeFilterable;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.sl.ue.entity.sys.SysUser;
 import com.sl.ue.service.sys.SysUserService;
+import com.sl.ue.util.http.Result;
 
+/**
+ * 说明 [用户]
+ * @author lxt
+ */
 @Controller
 @RequestMapping("/sysUser")
 public class SysUserWeb {
@@ -26,5 +29,29 @@ public class SysUserWeb {
 	public String findOne(){
 		List<SysUser> list = sysUserService.baseFindList(new SysUser());
 		return JSON.toJSONString(list, SerializerFeature.WriteMapNullValue);
+	}
+	
+	/**
+	 * 说明 [登录]
+	 * @author lxt
+	 */
+	public String login(String userNo, String userPwd){
+		Result result = new Result();
+		SysUser model = new SysUser();
+		model.setUserNo(userNo);
+		model.setUserPwd(userPwd);
+		List<SysUser> list = sysUserService.baseFindList(model);
+		//账号密码正确 允许登录，并返回相应数据
+		if(list.size()>0){
+			SysUser t = list.get(0);
+			SysUser resultUser = new SysUser();
+			resultUser.setUserNo(userNo);
+			resultUser.setUserName(t.getUserName());
+			result.setData(resultUser);
+			return result.toString();
+		}
+		result.setState("error");
+		return result.toString();
+	
 	}
 }
--- HjSystem/src/test/java/com/sl/ue/Test.java
+++ HjSystem/src/test/java/com/sl/ue/Test.java
@@ -1,5 +1,25 @@
 package com.sl.ue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.sl.ue.util.http.Result;
+
 public class Test {
 
+	public static void main(String[] args){
+		Result rs = new Result();
+//		rs.setState(0);
+//		rs.setMessage("你好");
+		
+		List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("aa", 111);
+		map.put("bb", "你好啊");
+		list.add(map);
+		rs.setData(list);
+		System.out.println(rs.toString());
+	}
 }
